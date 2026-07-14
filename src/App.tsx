eddca48fbc76de8435cc7880e74e7fe3e2308931
kthurm <karalthurmond@gmail.com>
2026-07-13 20:45:44 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300">
+      <div className="text-4xl lg:text-8xl font-bold text-gray-900">
+        Earthquake Dashboard
+      </div>
+    </div>
+  );
+}
+
+export default App;
