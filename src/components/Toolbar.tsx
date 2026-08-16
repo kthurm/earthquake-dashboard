7fe1f7f24a94b82caa3e5cda71b72f355a605e00
kthurm <karalthurmond@gmail.com>
2026-08-16 09:01:05 -0400

    Add map button

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,11 +1,9 @@
-import type { SortOption } from "../types/sortOption";
-
 interface ToolbarProps {
   setTimeRange: (timeRange: string) => void;
   timeRange: string;
   title: string;
-  sortBy: SortOption;
-  setSortBy: (sortBy: SortOption) => void;
+  view: "table" | "map";
+  setView: (view: "table" | "map") => void;
 }
 
 function Toolbar(props: ToolbarProps) {
@@ -15,7 +13,7 @@ function Toolbar(props: ToolbarProps) {
     <aside className="flex md:fixed justify-between items-center bg-primary text-white w-full p-3 md:p-5 drop-shadow-xl/25">
       <h1 className="tracking-[.3rem] uppercase">{props.title}</h1>
       <div className="flex gap-2">
-        <div className="flex flex-col items-center">
+        <div className="flex items-center space-x-2">
           <button
             className="text-white hover:text-secondary-dark hover:bg-white p-1.5 rounded text-center min-w-32 drop-shadow-xl/10 border text-xs"
             onClick={() => {
@@ -28,6 +26,14 @@ function Toolbar(props: ToolbarProps) {
             }}
           >
             Change Range to {buttonLabel}
+          </button>
+          <button
+            className="text-white hover:text-secondary-dark hover:bg-white p-1.5 rounded text-center min-w-22 drop-shadow-xl/10 border text-xs"
+            onClick={() =>
+              props.setView(props.view === "table" ? "map" : "table")
+            }
+          >
+            {props.view === "table" ? "View Map" : "View Table"}
           </button>
         </div>
       </div>
