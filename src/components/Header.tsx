3dda2df25d21ca761bc9e91f51931c597802403f
kthurm <karalthurmond@gmail.com>
2026-08-16 09:01:51 -0400

    Change font size and poition

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ interface HeaderProps {
 function Header({ title, count }: HeaderProps) {
   return (
     <header className="w-full lg:max-w-4xl mx-auto p-3 md:p-5 mt-2">
-      <p className="text-xl text-primary font-bold">
+      <p className="text-2xl text-center text-primary font-bold">
         {title}:{" "}
-        <span className="text-secondary-dark uppercase text-base">
+        <span className="text-secondary-dark uppercase text-lg">
           {count.toLocaleString()} earthquakes
         </span>
       </p>
